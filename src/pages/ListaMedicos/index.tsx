53fffb8794e34452007683db3802d425045137c2
elvisthermo <elvis.thermo@gmail.com>
2021-01-28 00:28:55 -0300

    listar medicos e medicos

--- src/pages/ListaMedicos/index.tsx
+++ src/pages/ListaMedicos/index.tsx
@@ -21,6 +21,7 @@ interface Medico {
 const ListaMedicos: React.FC = () => {
 
   const [medicos,setMedicos] = useState();
+  const [especialidades,setEspecialidades] = useState();
 
   useEffect(()=>{
     api.get('/medico/').then((response) => {
@@ -28,7 +29,33 @@ const ListaMedicos: React.FC = () => {
       console.log(medicosResponse)
       setMedicos(medicosResponse);
     });
+
+    api.get('/especialidade/').then((response) => {
+      const especialidadesResponse = response.data;
+      console.log(especialidadesResponse)
+      setEspecialidades(especialidadesResponse);
+    });
+
   },[]);
+
+  function getEspecialidade(crm) {
+    const especialidade = medicos && medicos.map(medico => {
+      if (medico.crm === crm) {
+        return getEspecialidadeNome(medico.especialidade);
+      }
+    });
+    return especialidade;
+  }
+
+  function getEspecialidadeNome(id) {
+    const especialidadeNome = especialidades && especialidades.map(especialidade => {
+      if (especialidade.id === id) {
+        return especialidade.tipo;
+      }
+    });
+    return especialidadeNome
+  }
+
 
 
   async function popUpDeletar(id:String,tipo:String) {
@@ -95,17 +122,16 @@ const ListaMedicos: React.FC = () => {
                   <tr key={medico.crm} className="table-row">
                     <td className="crm">{medico.crm?medico.crm:"crm teste"}</td>
                     <td className="nome">{medico.nome}</td>
-                    <td className="area_atuacao">{medico.area_atuacao}</td>
+                    <td className="area_atuacao">{getEspecialidade(medico.crm)}</td>
                     <td className="cpf">{medico.cpf}</td>
                     <td className="email">{medico.email}</td>
-                    <td className="contato">{medico.contato}</td>
+                    <td className="contato">{medico.telefone}</td>
                     <div className="buttons" style={{display: 'flex'}}>
                       <button className="myButton" id="remove" onClick={e => popUpDeletar(medico.crm,'CRM')}><FiTrash2 size={20} /><span className="tooltip-text">Remover</span></button>
                     </div>
                   </tr>
                 )
-              )
-              }
+              )}
               </tbody>
             </table>
           </div>
